922ccf9cca88557ea57417ff0266f1a0ab9f55cf
why@111454 <1114545339@qq.com>
2014-10-30 19:00:56 +0800

    commit  by   why 准备解析数据

---
+++ src/com/fgc/autocall/Tools/getData.java
@@ -0,0 +1,19 @@
+package com.fgc.autocall.Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fgc.autocall.app.json.SqNameEntity;
+
+public class getData {
+	
+	
+	public static List<String> getUserlist(){
+		List<String> sqnamelist=new ArrayList<String>();
+		sqnamelist.add("aa");
+		sqnamelist.add("bb");
+		sqnamelist.add("cc");
+		return  sqnamelist;
+	}
+
+}
